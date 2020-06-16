77d404301e725e095dc7ad29d0935b44558d4c21
ksk3164 <ksk3164@ex2i.com>
2020-06-16 12:22:46 +0900

    first commit

---
+++ app/src/main/java/com/ex2i/fireflysns/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.ex2i.fireflysns
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
