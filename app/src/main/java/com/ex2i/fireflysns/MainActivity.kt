01ad518c9d8d193899b091d7d4f4d60afa3b4ac4
ksk3164 <ksk3164@ex2i.com>
2020-06-26 10:17:25 +0900

    first commit

--- app/src/main/java/com/ex2i/fireflysns/MainActivity.kt
+++ app/src/main/java/com/ex2i/fireflysns/MainActivity.kt
@@ -2,11 +2,18 @@ package com.ex2i.fireflysns
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+
     }
 }
